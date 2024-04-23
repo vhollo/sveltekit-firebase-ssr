981102c28c396dcc9a62b232cb3e77b6c23e3173
vhollo <vhollo@protonmail.com>
2024-04-23 23:04:33 +0200

    userCountData fix

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,7 +2,7 @@ import type { PageLoad } from './$types';
 import { getUserCountData } from './Counter.svelte';
 export const load: PageLoad = async function load({ fetch, parent }) {
 	const parentData = await parent();
-	let userCountData = parentData.userSession ? await getUserCountData(fetch) : undefined;
+	let userCountData = parentData.userSession ? await getUserCountData(fetch) : 0;
 
 	return {
 		userCountData
